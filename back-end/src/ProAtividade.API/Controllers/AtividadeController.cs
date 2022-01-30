8d2e434165bfe5fddc2710330323d1d7f297d0cf
Rubens <rubenslemos@gmail.com>
2022-01-30 10:20:20 -0300

    fim seção 2 - introdução ao  .NET Core 5

---
+++ back-end/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Models;
+
+namespace ProAtividade.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtividadeController : ControllerBase
+    {
+        public IEnumerable<Atividade> Atividades = new List<Atividade>() {
+                new Atividade(2),
+                new Atividade(4),
+                new Atividade(6)
+            };
+        [HttpGet]
+        public IEnumerable<Atividade> Get()
+        {
+            return Atividades;
+        }        
+        
+        [HttpGet("{id}")]
+        public Atividade Get(int id)
+        {
+            return Atividades.FirstOrDefault(ati => ati.Id == id);
+        }        
+        
+        [HttpPost]
+        public IEnumerable<Atividade> Post(Atividade atividade)
+        {
+            return Atividades.Append<Atividade>(atividade);
+        }        
+        
+        [HttpPut ("{id}")]
+        public Atividade  Put(int id, Atividade atividade)
+        {
+            atividade.Id =atividade.Id+id;
+            return atividade;
+        }        
+        
+        [HttpDelete ("{id}")]
+        public string Delete(int id)
+        {
+            return $"meu primeiro método Delete {id}";
+        }
+    }
+}
